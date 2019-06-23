c82444121a4f1656c3bbe09292e17ab42aacb030
Mark Harrison <mhworld11@icloud.com>
2019-06-23 13:34:55 -0700

    creates signup file

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Signup from "./views/Signup.vue";
+
 
 Vue.use(Router)
 
@@ -20,6 +22,7 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
+    { path: "/signup", name: "signup", component: Signup }
   ]
-})
+});
