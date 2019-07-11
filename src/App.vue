3ee4a27b9b4dc38dd0681a7028a0f2fdb8e730ee
Mark Harrison <mhworld11@icloud.com>
2019-07-11 10:47:38 -0700

    implements theme

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,113 @@
 <template>
   <div id="app">
-  
-   
 
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <!-- <router-link to="/about">About</router-link>  -->
-      <router-link v-if="!isLoggedIn()" to="/signup">Signup</router-link> |
-      <router-link v-if="isLoggedIn()" to="/logout">Logout</router-link> 
+    <header class="kotha-menu marketing-menu">
+      <nav class="navbar  navbar-default">
+        <div class="container">
+          <div class="menu-content">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                      data-target="#myNavbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+              <ul class="top-social-icons list-inline pull-right">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                <li class="top-search">
+                  <a href="#" class="sactive"><i class="fa fa-search"></i></a>
+                </li>
+              </ul>
+              <ul class="nav navbar-nav text-uppercase pull-left">
+                <li><router-link to="/">Home</router-link></li>
+                <li v-if="!isLoggedIn()"><router-link to="/signup">Signup</router-link></li>
+                <li v-if="isLoggedIn()"><router-link to="/logout">Logout</router-link></li>
+                <li v-else><router-link to="/login">Login</router-link></li>
+                <li><router-link to="/users/">Your Hub</router-link></li>
+                <li><router-link to="/meditations">Mindfulness Mode</router-link></li>
+                <li><router-link to="/articles">News Room</router-link></li>
+              </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+            <div class="show-search">
+              <form role="search" method="get" id="searchform" action="#">
+                <div>
+                  <input type="text" placeholder="Search and hit enter..." name="s" id="s">
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <!-- /.container-fluid -->
+      </nav>
+    </header>
 
-      <router-link v-else to="/login">Login</router-link> |
-      
-      <router-link to="/users/:id">Your Hub</router-link> |
-      <!-- <router-link to="/users/:id/edit">Edit Profile</router-link> |  -->
-      <router-link to="/meditations">Mindfulness Mode</router-link> |
-      <router-link to="/articles">News Room</router-link> |
-      <!-- <router-link v-if="!isLoggedIn()" to="/spotify_authorize">Sign In With Spotify</router-link> -->
-    </div>
+
     <router-view/>
  
-
-  
+    <footer>
+      <div class="container">
+        <div class="footer-widget-row">
+          <div class="footer-widget contact-widget">
+            <div class="widget-title">
+              <img src="assets/images/ft-logo.png" alt="">
+            </div>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+              ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eaccusam et justo duo
+              dolores eteem.</p>
+            <div class="address">
+              <h4 class="text-uppercase">contact Info</h4>
+              <p> 239/2 NK Street, DC, USA</p>
+              <p> Phone: +123 456 78900</p>
+              <a href="mailto:theme@kotha.com">theme@kotha.com</a>
+            </div>
+          </div>
+          <div class="footer-widget twitter-widget">
+            <h2 class="widget-title text-uppercase">
+              Latest TWeeTs
+            </h2>
+            <div class="single-tweet">
+              <p>Check our new theme 'kotha - Personal WordPress Blog Theme' on #themeforest #Envato
+                #WordPress <br>
+                <a href="https://t.co/kN5OPEuPzx">https://t.co/kN5OPEuPzx</a></p>
+              <h4><i class="fa fa-twitter"></i>Tweeted on 17 hours ago</h4>
+            </div>
+            <div class="single-tweet">
+              <p>Check our new theme 'kotha - Personal WordPress Blog Theme' on #themeforest #Envato
+                #WordPress
+                <br>
+                <a href="https://t.co/kN5OPEuPzx">https://t.co/kN5OPEuPzx</a></p>
+              <h4><i class="fa fa-twitter"></i>Tweeted on 17 hours ago</h4>
+            </div>
+          </div>
+          <div class="footer-widget social-widget">
+            <h2 class="widget-title text-uppercase">
+              Featured video
+            </h2>
+            <div class="footer-video">
+              <iframe width="300" height="250" src="https://www.youtube.com/embed/VTlXttQL_Yk"
+                      allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid text-center ft-copyright">
+        <p>&copy; 2017 <a href="">Kotha PRO</a> - Designed with <i class="fa fa-heart"></i> by <a
+          href="http://shapedtheme.com">ShapedTheme</a></p>
+      </div>
+    </footer>
+    
 
  
-</div>
+  </div>
 </template>
 <script>
 export default {
@@ -40,26 +124,4 @@ export default {
 
 </script>
 
-<style>
 
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/views/Meditations/Index.vue
+++ src/views/Meditations/Index.vue
@@ -18,10 +18,44 @@
       <button class="button play" @click.prevent="playSound(meditation.sound_url)">
         <span class="fa fa-play-circle-o"></span>
       </button><br>
+      <!-- <button class="button play" @click.prevent="pauseSound(meditation.sound_url)">
+        <span class="fa fa-play-circle-o"></span>
+      </button><br> -->
       <p>Favorite</p>
       <button class="button play" @click.prevent="toggleFavorite(meditation.id)">
         <span class="fa fa-play-circle-o"></span>
       </button>
+    </div>
+
+    <div class="kotha-default-content">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="row">
+              <div v-for="meditation in meditations" class="col-md-4">
+                <article class="portfolio portfolio-2 post-grid">
+                  <div class="post-thumb">
+                    <a href=" "><img src="assets/images/gl-1.jpg" alt=""></a>
+                    <div class="post-thumb-overlay text-center">
+                      <div class="text-uppercase text-center">
+                        <a href="single-portfolio.html"><i class="fa fa-link"></i></a>
+                        <a href="assets/images/gl-1.jpg" data-rel="prettyPhoto[gallery]"><i
+                          class="fa fa-search"></i></a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="post-content">
+                    <header class="entry-header text-center text-uppercase">
+                      <h6><a href="#"> craft</a></h6>
+                      <h2 class="entry-title"><a href=" ">{{ meditation.title }}</a></h2>
+                    </header>
+                  </div>
+                </article>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
    
   </div>
@@ -33,7 +67,8 @@ export default {
     return {
       userMeditations: [],
       meditations: [],
-      articles: []
+      articles: [],
+      audioPlay: false
     };
   },
   created: function() {
@@ -43,25 +78,33 @@ export default {
     });
   },
   methods: {
+
+    // pauseSound(sound) {
+
+    //   if (sound) {
+    //     var audio = new Audio(sound);
+    //     audio.pause();
+    //   }
+    // },
+    // playSound(sound) {
+
     playSound(sound) {
-      if (sound) {
-        var audio = new Audio(sound);
-        audio.play();
+      if (!this.audio) {
+        this.audio = new Audio(sound);
+      }
+      if (this.audioPlay === false) {
+        this.audio.play();
+        this.audioPlay = true;
+      } else {
+        this.audio.pause();
+        this.audioPlay = false;
       }
     },
-    pauseSound(sound) {
-      if (sound) {
-        var audio = new Audio(sound);
-        audio.play();
-      }
-    },
+
     toggleFavorite(medId) {
       console.log(medId);
-
-
       var params = {
         meditation_id: medId
-         
       };
       console.log(params);
       axios
